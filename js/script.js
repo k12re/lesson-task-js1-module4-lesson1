ded2e6033f935cfce8b6936dbb90c9a2a5c75496
Ken Thore <kenthore@live.no>
2022-08-29 23:58:16 +0200

    fixed formatting after adding try/catch

--- js/script.js
+++ js/script.js
@@ -8,43 +8,43 @@ let resultsContainer = document.querySelector(".results");
 async function callAPI() {
 
     try {
-    const response = await fetch(url);
+        const response = await fetch(url);
 
-    const data = await response.json();
+        const data = await response.json();
 
-    // always log and inspect the data you get from an API call to see what properties it has
-    console.log(data);
+        // always log and inspect the data you get from an API call to see what properties it has
+        console.log(data);
 
-    const teams = data
+        const teams = data
 
-    resultsContainer.innerHTML = "";
+        resultsContainer.innerHTML = "";
 
-    for (let i = 0; i < teams.length; i++) {
-        // we only want to display a maximum of 15 teams
-        // there will be less than 15 if any team names begin with "C"
-        // use break to leave the loop
-        if (i === 15) {
-            break
-        };
+        for (let i = 0; i < teams.length; i++) {
+            // we only want to display a maximum of 15 teams
+            // there will be less than 15 if any team names begin with "C"
+            // use break to leave the loop
+            if (i === 15) {
+                break
+            };
 
-        const teamName = teams[i]. teamName;
-        const city = teams[i].location;
+            const teamName = teams[i]. teamName;
+            const city = teams[i].location;
 
-        // we are checking for small "c" and big "C"
-        // instead of checking for both small "c" and big "C" we can make the teamName lowercase and just check for "c"
-        if (teamName.toLowerCase().startsWith("c") /*|| teamName.startsWith("C")*/) {
-            continue
-        };
+            // we are checking for small "c" and big "C"
+            // instead of checking for both small "c" and big "C" we can make the teamName lowercase and just check for "c"
+            if (teamName.toLowerCase().startsWith("c") /*|| teamName.startsWith("C")*/) {
+                continue
+            };
         
-        resultsContainer.innerHTML += `<div class="card">
+            resultsContainer.innerHTML += `<div class="card">
                                         <h4>${teamName}</h4>
                                         <p>${city}</p>
                                         </div>`;
+        }
+    } catch (error) {
+        console.log(error);
+        resultsContainer.innerHTML = message("error", error)
     }
-} catch (error) {
-    console.log(error);
-    resultsContainer.innerHTML = message("error", error)
-}
 
 }
 
